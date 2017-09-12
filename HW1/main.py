5dbcf78f027452ff93bd1a0c8a619accb5ee1444
stevenliuyi <stevenliuyi@gmail.com>
2017-09-12 14:27:34 -0400

    add stratified Monte Carlo

--- HW1/main.py
+++ HW1/main.py
@@ -1,16 +1,18 @@
 import mc_integration as mci
 
 def u1(y):
-    return 0.5 - (y[0]-1/2)**2-(y[1]-1/2)**2
+    return 0.5 - (y[0]-0.5)**2 - (y[1]-0.5)**2
 
 def u2(y):
     delta = 0.1
     return 1/(abs(0.3-y[0]**2-y[1]**2) + delta)
 
-n_samples = [10, 100, 1000, 2000, 5000, 10000]
+n_samples = [100, 1000, 2000, 5000, 10000]
 
-estimator = mci.PlainMC(u1)
+estimators = [mci.PlainMC(u1),
+              mci.StratifiedMC(u1)]
 
-for n in n_samples:
-    estimator.run(n)
-    estimator.print_results()
+for estimator in estimators:
+    for n in n_samples:
+        estimator.run(n)
+        estimator.print_results()
--- HW1/mc_integration.py
+++ HW1/mc_integration.py
@@ -1,15 +1,17 @@
 import numpy as np
 
 class MCIntegration:
-    def __init__(self, func, samples_generator):
+    def __init__(self, func, samples_generator, name):
         self.func = func
         self.samples_generator = samples_generator
+        self.name = name
     
     def print_results(self):
-        print("===========================")
+        print("=====================================")
+        print(self.name)
         print("number of samples: %d" % self.n_samples)
-        print("mean of estimator: %.8f" % self.mean)
-        print("variance of estimator: %.8f" % self.variance)
+        print("mean of estimator: %.8e" % self.mean)
+        print("variance of estimator: %.8e" % self.variance)
 
     def onetime_run(self, n_samples):
         # generate samples
@@ -38,14 +40,51 @@ class MCIntegration:
 # Plain Monte Carlo estimator
 class PlainMC(MCIntegration):
     def __init__(self, func):
-        MCIntegration.__init__(self, func, PlainMC.samples_generator)
+        name = 'Plain Monte Carlo estimator'
+        MCIntegration.__init__(self, func, self.samples_generator, name)
 
     @staticmethod
     def samples_generator(n_samples):
         samples = []
         for i in range(n_samples):
             sample = np.zeros(2)
-            for d in range(len(sample)):
-                sample[d] = np.random.uniform(0, 1)
+            for dim in range(len(sample)):
+                sample[dim] = np.random.uniform(0, 1)
             samples.append(sample)
+        return samples
+
+# Stratified Monte Carlo estimator
+class StratifiedMC(MCIntegration):
+    def __init__(self, func, n_strata=100):
+        # number of strata
+        self.n_strata = n_strata
+        name = 'Stratified Monte Carlo estimator'
+        MCIntegration.__init__(self, func, self.samples_generator, name)
+
+    def samples_generator(self, n_samples):
+        # probability associated with each stratum for uniform grid
+        prob = 1.0/self.n_strata
+
+        # number of samples per starta
+        m = int(prob * n_samples)
+
+        # partition
+        partition = np.linspace(0, 1, num=int(np.sqrt(self.n_strata))+1)
+
+        samples = []
+
+        for ix in range(len(partition)-1):
+            for iy in range(len(partition)-1):
+                x_left = partition[ix]
+                x_right = partition[ix+1]
+
+                y_left = partition[iy]
+                y_right = partition[iy+1]
+
+                for i in range(m):
+                    sample = np.zeros(2)
+                    sample[0] = np.random.uniform(x_left, x_right)
+                    sample[1] = np.random.uniform(y_left, y_right)
+                    samples.append(sample)
+        
         return samples
